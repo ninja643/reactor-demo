3e07afebce45fcc2dae54551194a45d001031dbc
Marko Milosevic <mmilosevic@wcc-group.com>
2020-03-24 10:00:02 +0100

    More examples

--- src/main/java/rs/ac/ni/pmf/marko/reactor/ReactorDemo.java
+++ src/main/java/rs/ac/ni/pmf/marko/reactor/ReactorDemo.java
@@ -1,10 +1,17 @@
 package rs.ac.ni.pmf.marko.reactor;
 
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 public class ReactorDemo
 {
 	public static void main(String[] args)
+	{
+		doOnNextWithMono();
+		simpleDoOnNext();
+	}
+
+	public static void simpleDoOnNext()
 	{
 		final Flux<Integer> evenNumbers = Flux.range(1, 10)
 			.filter(value -> value % 2 == 0);
@@ -37,5 +44,38 @@ public class ReactorDemo
 			.subscribe(number -> {
 				System.out.println("Number: " + number);
 			});
+	}
+
+	public static void doOnNextWithMono()
+	{
+		final Flux<Integer> evenNumbers = Flux.range(1, 10)
+			.filter(value -> value % 2 == 0);
+
+		evenNumbers
+			.doOnNext(number -> {
+				doSomethingWithTheNumber(number)
+					.subscribe(num -> {
+						System.out.println("Handled the number " + num + " in subscribe");
+					});
+			})
+			.subscribe(number -> {
+				System.out.println("Number: " + number);
+			});
+	}
+
+	private static Mono<Integer> doSomethingWithTheNumber(int number)
+	{
+		System.out.println("Gonna do something with " + number);
+		try
+		{
+			Thread.sleep(1000);
+		}
+		catch (InterruptedException e)
+		{
+			e.printStackTrace();
+		}
+
+		System.out.println("Done something with " + number);
+		return Mono.just(number);
 	}
 }
